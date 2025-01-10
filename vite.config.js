f9ded14794a75f100376d9d57360cc4b68ea2508
Oladimeji402 <olamoney665@gmail.com>
2025-01-10 16:22:58 +0100

    updated commit

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,6 @@ export default defineConfig({
     },
   },
   build: {
-    chunkSizeWarningLimit: 10000, // Increase the limit to 1000 KB (1 MB)
+    chunkSizeWarningLimit: 5000, // Increase the limit to 1000 KB (1 MB)
   },
 });
